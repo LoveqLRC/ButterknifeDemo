54291c964357605b023fa080e3ca7502998acff4
LoveqLRC <664215432@qq.com>
2020-07-27 16:40:42 +0800

    删除测试代码

--- app/src/main/java/com/loveqrc/butterknifedemo/MainActivity.java
+++ app/src/main/java/com/loveqrc/butterknifedemo/MainActivity.java
@@ -20,13 +20,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        new MainActivity_ViewBinding().bind(this);
         btn.setText("Rc在努力");
     }
 }
 
-class MainActivity_ViewBinding {
-    public void bind(MainActivity target) {
-        target.btn= (Button) target.findViewById(R.id.btn);
-    }
-}
